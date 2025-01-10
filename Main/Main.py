d36f32959057cd495b3d5dd80ef2b7a63d3bb48a
Connor Bonn <Connorb809@lpsk12.org>
2025-01-10 12:32:33 -0700

    done

---
+++ Main/Main.py
@@ -0,0 +1,12 @@
+import time
+
+sentance = "hello my name is sam i like eating green eggs and ham"
+def Main(x): 
+    print("Please type out this sentance: ", x)
+    start = time.time()
+    user_input = input()
+    end = time.time()
+    print(12 / float("end - start"))
+
+##credit to  @cjhveal for the list of words
+Main(sentance)
